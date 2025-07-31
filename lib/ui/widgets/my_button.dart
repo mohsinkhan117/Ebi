5a2a9e9d06cc7e9927c7019ba7a86c7d6cfbe0df
Azaz Ahmad Khan <azazahmadkhan264@gmail.com>
2025-07-31 10:59:27 +0500

    folder structure changed

---
+++ lib/ui/widgets/my_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final textChild;
+  void Function()? onTap;
+
+  MyButton({required this.textChild, required this.onTap, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        margin: EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(4),
+        ),
+
+        child: Center(
+          child: Text(textChild, style: TextStyle(color: Colors.pinkAccent)),
+        ),
+      ),
+    );
+  }
+}
